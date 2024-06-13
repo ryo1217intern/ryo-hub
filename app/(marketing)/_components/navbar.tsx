61459d12e6c6a3adb58a33e6d0debfbf9e3fa756
ryosuke <ryosuke@dev-ryo.com>
2024-06-14 03:18:44 +0900

    feat: 🎸 mode-togglenの設置

---
+++ app/(marketing)/_components/navbar.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from '@/components/mode-toggle'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <ModeToggle />
+    </div>
+  )
+}
+
+export default Navbar
